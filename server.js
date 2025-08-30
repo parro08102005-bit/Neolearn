8ce664e30ea4062a5767ab8b8b039e76cd8ef18c
parro08102005-bit <parro08102005@gmail.com>
2025-08-30 18:59:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ app.use(express.static(path.join(__dirname, "dist")));
 
 // SPA fallback
 app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 // ---------- Start ----------
