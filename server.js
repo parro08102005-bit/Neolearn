46102f93c337996ec47b4380078ea4c093c9ee38
parro08102005-bit <parro08102005@gmail.com>
2025-08-31 22:03:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 // ---------- Google OAuth client init ----------
-const creds = JSON.parse(process.env.GOOGLE_CLIENT_SECRET);
+const creds = require("./client_secret.json");   // direct JSON file load
 const googleClient = new OAuth2Client(creds.web.client_id);
-
 // ---------- MongoDB ----------
 const uri = process.env.MONGODB_URI;
 if (!uri) {
