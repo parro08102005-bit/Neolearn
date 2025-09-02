b1d3555032338c01ed80daf77bd70ce608c0b90d
parro08102005-bit <parro08102005@gmail.com>
2025-09-02 19:33:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, trim: true, lowercase: true, unique: true },
-    phone: { type: String, required: true, trim: true },
+    phone: { type: String, required: true, trim: false },
     gender: { type: String, enum: ["male", "female", "other"], required: true },
     passwordHash: { type: String },
     otp: { type: String }, // ✅ OTP field
